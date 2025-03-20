199fb9954d5b8d668f9d1fdd07b71c9cddd4fd37
git-shiivu <shivamku9107@gmail.com>
2025-03-20 20:48:18 +0530

    remove h-[35vh] class from div

--- App.jsx
+++ App.jsx
@@ -15,8 +15,8 @@ function App() {
             <span className='text-[2.5vw] cursor-pointer duration-500 hover:text-green-600'>Contact</span>
           </div>
           <div className='inline-flex m-[4vw]'>
-            <img className='w-[30vw] h-[35vh]' src="https://tint.creativemarket.com/-fc2O8L6UQr741mlhB4cXKO-iCoIFi9Zha4rZ_Lqyc4/width:1820/height:1214/gravity:nowe/rt:fill-down/el:1/czM6Ly9maWxlcy5jcmVhdGl2ZW1hcmtldC5jb20vaW1hZ2VzL3NjcmVlbnNob3RzL3Byb2R1Y3RzLzg3Mi84NzIxLzg3MjE0NTQvc3BhY2UtdHJhdmVsLW8ucG5n?1594759552" alt="" />
-            <p className=' font-sans text-[1.5vw] h-[35vh] w-[70vw] p-[1vw] border-l-[1vw] bg-orange-50'>
+            <img className='w-[30vw]' src="https://tint.creativemarket.com/-fc2O8L6UQr741mlhB4cXKO-iCoIFi9Zha4rZ_Lqyc4/width:1820/height:1214/gravity:nowe/rt:fill-down/el:1/czM6Ly9maWxlcy5jcmVhdGl2ZW1hcmtldC5jb20vaW1hZ2VzL3NjcmVlbnNob3RzL3Byb2R1Y3RzLzg3Mi84NzIxLzg3MjE0NTQvc3BhY2UtdHJhdmVsLW8ucG5n?1594759552" alt="" />
+            <p className=' font-sans text-[1.5vw] w-[70vw] p-[1vw] border-l-[1vw] bg-orange-50'>
             Embark on an extraordinary journey beyond Earth's embrace and witness the cosmos unfold before your very eyes. With us, you'll explore the wonders of space, from the thrill of weightlessness to the awe-inspiring views of our planet from orbit. Our space travel website offers a gateway to unforgettable experiences, scientific discoveries, and the boundless potential of human exploration, all while pushing the boundaries of what's possible. Prepare to be captivated by the stars!
             </p>
           </div>
